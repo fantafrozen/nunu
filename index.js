689598b072461ee1682e562e4c07f75c79f8b282
fantafrozen <143635969+fantafrozen@users.noreply.github.com>
2023-08-31 02:27:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ api.search("chinese").then((list) => {
         console.log(e.title)
         api.page(e.link, ['title','download_urls']).then(r=>{
            // exec(`wget -c -O "./output/${e.title}.m3u8" "${e.download_urls["1080p"]}"`)
-            exec(`ffmpeg -i "${r.download_urls["1080P"]}.m3u8" -c copy "./output/${e.title}.mp4"`)
+            exec(`ffmpeg -i "${r.download_urls["1080P"]}.m3u8" -c copy "./output/${e.title}.mp4"`, (a,b,c) => console.log(a,b,c))
     
         });
     })
