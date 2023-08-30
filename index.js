25be90cb108370b1d138f3141c8d56168c5003c8
fantafrozen <143635969+fantafrozen@users.noreply.github.com>
2023-08-31 02:49:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const api = require("@justalk/pornhub-api") 
 const { exec } = require("child_process")
+var m3u8ToMp4 = require("m3u8-to-mp4");
+var converter = new m3u8ToMp4();
 const fs = require("fs")
 var counter = false;
 api.search("chinese").then((list) => {
@@ -9,10 +11,12 @@ api.search("chinese").then((list) => {
         if(counter) return;
         counter = true;
         console.log(e.title)
-        api.page(e.link, ['title','download_urls']).then(r=>{
-           // exec(`wget -c -O "./output/${e.title}.m3u8" "${e.download_urls["1080p"]}"`)
-            exec(`ffmpeg -i "${r.download_urls["1080P"]}.m3u8" -c copy "./output/${e.title}.mp4"`, (a,b,c) => console.log(a,b,c))
-    
+        api.page(e.link, ['title','download_urls']).then(async r=>{
+          //  exec(`wget -c -O "./output/${e.title}.m3u8" "${r.download_urls["1080P"]}"`,(a,b,c)=>
+          await converter
+          .setInputFile(r.download_urls["1080P"])
+          .setOutputFile("./output/"+e.title+".mp4")
+          .start();
         });
     })
 })
