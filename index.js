3dcafbe251b9ade5f5217bf6c995d9b68783a29b
fantafrozen <143635969+fantafrozen@users.noreply.github.com>
2023-08-31 02:13:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const api = require("@justalk/pornhub-api") 
 const { exec } = require("child_process")
 const fs = require("fs")
-var counter = 0
+var counter = false;
 api.search("chinese").then((list) => {
     list.results.forEach(e => {
         if(e.premium) return;
         if(fs.existsSync(`./output/${e.title}.mp4`))return;
-        if(counter > 4)return;counter++;
+        if(counter) return;
+        counter = true;
         api.page(e.link, ['title','download_urls']).then(r=>{
            // exec(`wget -c -O "./output/${e.title}.m3u8" "${e.download_urls["1080p"]}"`)
             exec(`ffmpeg -i "${r.download_urls["1080P"]}.m3u8" -c copy "./output/${e.title}.mp4"`)
